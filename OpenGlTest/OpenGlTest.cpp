561339b78e52adfea4c63a4fe5da0896c806f35e
CashMoneyLee <hune0725@gmail.com>
2024-10-15 15:08:23 +0900

    solar system

--- OpenGlTest/OpenGlTest.cpp
+++ OpenGlTest/OpenGlTest.cpp
@@ -1,163 +1,35 @@
 ﻿#include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <cmath> // cos, sin 함수 사용을 위해 추가
 
-int FlatShaded = 0;
-int Wireframed = 0;
+static int Day = 0, Time = 0;
 
-int ViewX = 0, ViewY = 0;
-
-void InitLight() {
-    GLfloat mat_diffuse[] = { 0.5, 0.4, 0.3, 1.0 };
-    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat mat_ambient[] = { 0.5, 0.4, 0.3, 1.0 };
-    GLfloat mat_shininess[] = { 15.0 };
-    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-    GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };
-    GLfloat light_ambient[] = { 0.3, 0.3, 0.3, 1.0 };
-    GLfloat light_position[] = { -3, 6, 3.0, 0.0 };
-    glShadeModel(GL_SMOOTH);
-    glEnable(GL_DEPTH_TEST);
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
+void SetLighting() {
+    GLfloat light_position[] = { 0.0, 1.5, 1.0, 1.0 };
+    GLfloat light_diffuse[] = { 1.0, 1.0, 1.0, 1.0 };
     glLightfv(GL_LIGHT0, GL_POSITION, light_position);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+}
+
+void SetMaterialColor(GLfloat r, GLfloat g, GLfloat b) {
+    GLfloat mat_diffuse[] = { r, g, b, 1.0 };
     glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-    glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
-    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 }
 
-void MyMouseMove(GLint X, GLint Y) {
-    ViewX = X;
-    ViewY = Y;
-    glutPostRedisplay();
-}
-
-void MyKeyboard(unsigned char key, int x, int y) {
-    switch (key) {
-    case 'q': case 'Q': case '\033':
-        exit(0);
-        break;
-    case 's':
-        if (FlatShaded) {
-            FlatShaded = 0;
-            glShadeModel(GL_SMOOTH);
-        }
-        else {
-            FlatShaded = 1;
-            glShadeModel(GL_FLAT);
-        }
-        glutPostRedisplay();
-        break;
-    case 'w':
-        Wireframed = !Wireframed;
-        glutPostRedisplay();
-        break;
+void DrawOrbit() {
+    glDisable(GL_LIGHTING); // 궤도는 조명 없이 그립니다
+    glColor3f(0.6, 0.6, 0.6); // 궤도는 회색으로 표현
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < 360; i++) {
+        GLfloat theta = (GLfloat)(i * 3.14159 / 180.0); // GLfloat로 변환
+        glVertex3f(0.7 * cos(theta), 0.0, 0.7 * sin(theta)); // 세 번째 인수 추가
     }
-}
-
-void DrawTable() {
-    // 테이블 상판
-    glPushMatrix();
-    glTranslatef(0.0, -0.5, -1.0); // 상판을 바닥에서 0.5 떨어지도록 수정
-    glScalef(1.0, 0.05, 1.0); // 테이블 상판의 크기 조정
-    if (Wireframed) {
-        glutWireCube(1.0);
-    }
-    else {
-        glutSolidCube(1.0);
-    }
-    glPopMatrix();
-
-    // 테이블 다리 (위치 수정)
-    float legHeight = 0.5; // 다리 높이
-    float legWidth = 0.05; // 다리 폭
-
-    // 첫 번째 다리
-    glPushMatrix();
-    glTranslatef(0.45, -0.75, -0.55); // 다리 위치 수정
-    glScalef(legWidth, legHeight, legWidth);
-    if (Wireframed) {
-        glutWireCube(1.0);
-    }
-    else {
-        glutSolidCube(1.0);
-    }
-    glPopMatrix();
-
-    // 두 번째 다리
-    glPushMatrix();
-    glTranslatef(-0.45, -0.75, -0.55);
-    glScalef(legWidth, legHeight, legWidth);
-    if (Wireframed) {
-        glutWireCube(1.0);
-    }
-    else {
-        glutSolidCube(1.0);
-    }
-    glPopMatrix();
-
-    // 세 번째 다리
-    glPushMatrix();
-    glTranslatef(0.45, -0.75, -1.45);
-    glScalef(legWidth, legHeight, legWidth);
-    if (Wireframed) {
-        glutWireCube(1.0);
-    }
-    else {
-        glutSolidCube(1.0);
-    }
-    glPopMatrix();
-
-    // 네 번째 다리
-    glPushMatrix();
-    glTranslatef(-0.45, -0.75, -1.45);
-    glScalef(legWidth, legHeight, legWidth);
-    if (Wireframed) {
-        glutWireCube(1.0);
-    }
-    else {
-        glutSolidCube(1.0);
-    }
-    glPopMatrix();
-}
-
-void DrawObjects() {
-    // 다양한 기하학적 물체 모델링
-    glPushMatrix();
-    glTranslatef(-0.3, -0.38, -1.0); // 탁자 상판 높이에 맞게 수정
-    if (Wireframed) {
-        glutWireSphere(0.1, 16, 16);
-    }
-    else {
-        glutSolidSphere(0.1, 16, 16);
-    }
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0.3, -0.38, -1.0); // 탁자 상판 높이에 맞게 수정
-    if (Wireframed) {
-        glutWireCube(0.2);
-    }
-    else {
-        glutSolidCube(0.2);
-    }
-    glPopMatrix();
-
-    // 원뿔을 90도 회전
-    glPushMatrix();
-    glTranslatef(0.0, -0.38, -0.7); // 탁자 상판 높이에 맞게 수정
-    glRotatef(-90, 1.0, 0.0, 0.0); // x축을 기준으로 90도 회전
-    if (Wireframed) {
-        glutWireCone(0.1, 0.2, 16, 16);
-    }
-    else {
-        glutSolidCone(0.1, 0.2, 16, 16);
-    }
-    glPopMatrix();
+    glEnd();
+    glEnable(GL_LIGHTING); // 조명 다시 활성화
 }
 
 void MyDisplay() {
@@ -165,38 +37,71 @@ void MyDisplay() {
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-    gluLookAt(0.0, 0.0, 2.0, 0.0, 0.0, -1.0, 0.0, 1.0, 0.0);
+    gluLookAt(0.0, 2.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0);
 
-    // 탁자 그리기
-    DrawTable();
+    SetLighting();
 
-    // 기하학적 물체 그리기
-    DrawObjects();
+    // 태양을 빨간색으로 표현
+    SetMaterialColor(1.0, 0.0, 0.0);
+    glutSolidSphere(0.2, 20, 16); // 태양 그리기
 
-    glFlush();
+    DrawOrbit();
+
+    glPushMatrix();
+    glRotatef((GLfloat)Day, 0.0, 1.0, 0.0);
+    glTranslatef(0.7, 0.0, 0.0);
+
+    // 지구를 파란색으로 표현
+    SetMaterialColor(0.0, 0.0, 1.0);
+    glutSolidSphere(0.1, 10, 8); // 지구 그리기
+
+    glPushMatrix();
+    glRotatef((GLfloat)Time, 0.0, 1.0, 0.0);
+    glTranslatef(0.2, 0.0, 0.0);
+
+    // 달을 노란색으로 표현
+    SetMaterialColor(1.0, 1.0, 0.0);
+    glutSolidSphere(0.04, 10, 8); // 달 그리기
+
+    glPopMatrix();
+    glPopMatrix();
+
+    glutSwapBuffers();
 }
 
-void MyReshape(int w, int h) {
-    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+void MyKeyboard(unsigned char key, int x, int y) {
+    switch (key) {
+    case 'd':
+        Day = (Day + 10) % 360;
+        glutPostRedisplay();
+        break;
+    case 't':
+        Time = (Time + 5) % 360;
+        glutPostRedisplay();
+        break;
+    default:
+        break;
+    }
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("OpenGL Sample Drawing with Lighting and Orbit");
+
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(45.0, (GLfloat)w / (GLfloat)h, 1.0, 10.0);
-}
+    glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 3.0);
 
-int main(int argc, char* argv[]) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
-    glutInitWindowSize(400, 400);
-    glutInitWindowPosition(0, 0);
-    glutCreateWindow("Table with Geometric Objects");
-
-    glClearColor(0.4, 0.4, 0.4, 0.0);
-    InitLight();
     glutDisplayFunc(MyDisplay);
     glutKeyboardFunc(MyKeyboard);
-    glutMotionFunc(MyMouseMove);
-    glutReshapeFunc(MyReshape);
     glutMainLoop();
+    return 0;
 }
 
 
